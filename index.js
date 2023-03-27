ac9c014cea2eadb209c9c12234f5ee535d36d112
civanozbay <civan417@hotmail.com>
2023-03-27 20:14:19 +0200

    done

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
+const {hash} = window.location;
+
+const message = atob(hash.replace('#',''))
+
+if(message){
+    document.querySelector('#message-form').classList.add('hide');
+    document.querySelector('#message').classList.remove('hide')
+
+    document.querySelector('h1').innerHTML =message;
+}
+
 document.querySelector('form').addEventListener('submit',event => {
     event.preventDefault(); // stop the default behaviour which submit the form the directly.Since we don't have any database its unneccessary
 
-    const input = document.querySelector('input');
+    document.querySelector('#message-form').classList.add('hide');
+    document.querySelector('#link-form').classList.remove('hide');
 
+    const input = document.querySelector('#message-input');
+    const encrypted = btoa(input);
 
-})
+    const linkInput = document.querySelector('#link-input')
+    linkInput.value=`${window.location}#${encrypted}`
+    linkInput.select();
+    
+});
+
