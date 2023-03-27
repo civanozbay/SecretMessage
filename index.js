457d068b63991f89f3494f5ec82863654437a4d0
civanozbay <civan417@hotmail.com>
2023-03-27 15:49:47 +0200

    submit form

---
+++ index.js
@@ -0,0 +1,7 @@
+document.querySelector('form').addEventListener('submit',event => {
+    event.preventDefault(); // stop the default behaviour which submit the form the directly.Since we don't have any database its unneccessary
+
+    const input = document.querySelector('input');
+
+
+})
